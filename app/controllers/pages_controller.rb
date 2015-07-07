9355594bfb0a78c3c5dbce87d9d6aa74a6445bc5
Cruz <nunez.a.cruz@gmail.com>
2015-07-07 16:23:12 -0400

    Merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,10 +9,7 @@ class PagesController < ApplicationController
     # @clip = current_.clips.build
   end
 
-<<<<<<< HEAD
-=======
   def musical_preferences
     @user = User.find_by_id(params[:id])
   end
->>>>>>> 7f622176ba43a98b09b7330fe97d57cf47096293
 end
