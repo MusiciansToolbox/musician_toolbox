9264454a1526720f30bfb21215aa97596c97bc67
TurnerMooreDavis <turnermdavis@gmail.com>
2015-07-08 16:53:43 -0400

    include user_id validation and add has_many to instrument model

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -5,7 +5,7 @@ class Clip < ActiveRecord::Base
   has_attached_file :uploaded_file
   validates_attachment_content_type :uploaded_file, :content_type => /\Aaudio\/.*\Z/
 
-  # validates :user_id, presence: true, numericality: true
+  validates :user_id, presence: true, numericality: true
   # validates :instrument_id, presence: true, numericality: true #work this in later
 
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,4 @@
 class Instrument < ActiveRecord::Base
   belongs_to :user
+  has_many :clips
 end
