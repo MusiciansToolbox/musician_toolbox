90232aff252f6d3381c5f0dbf02c190c4204b2ea
TurnerMooreDavis <turnermdavis@gmail.com>
2015-07-09 16:10:28 -0400

    merge commit

--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -1,7 +1,7 @@
 class UserSearch < ActiveRecord::Base
   belongs_to :user
 
-  def self.search_musicians(zipcode, genre_id, instrument_id)
+  def self.search_musicians(zipcode:, genre_id:, instrument_id:)
     local_users = User.where(zipcode:zipcode)
     p local_users
   end
