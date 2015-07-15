24db3f103106d8850c51d60c4c3bfee44449a953
TurnerMooreDavis <turnermdavis@gmail.com>
2015-07-15 09:50:36 -0400

    add code to clips to validate length

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -10,5 +10,20 @@ class Clip < ActiveRecord::Base
 
   validates :user_id, presence: true, numericality: true
   validates :instrument_id, presence: true, numericality: true
+  # before_uploaded_file_post_process :fetch_media_length
+  #
+  # validates :duration, :presence => true, :numericality => { :less_than_or_equal_to => 2.minutes }
+  #
+  # def fetch_media_length
+  #   duration_in_seconds = 1.0
+  #
+  #   begin
+  #     clip_file_path = uploaded_file.queued_for_write[:original].path
+  #
+  #     duration_in_seconds = FFMPEGWrapper.duration_in_seconds(clip_file_path)
+  #   end
+  #
+  #   self.duration = duration_in_seconds
+  # end
 
 end
