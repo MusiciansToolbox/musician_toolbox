98a61a4d151d6eaaa74a5a8a66b767c0d73d25ad
TurnerMooreDavis <turnermdavis@gmail.com>
2015-07-15 14:52:34 -0400

    set up transloadit upload test page

---
+++ app/helpers/transloadit_uploads_helper.rb
@@ -0,0 +1,2 @@
+module TransloaditUploadsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :transloadituploads
   resources :jam_circles
   resources :genres
   resources :instruments
