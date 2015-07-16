543c95e6b4bebf9c40c96558a17de3e9fa286a16
TurnerMooreDavis <turnermdavis@gmail.com>
2015-07-16 11:53:13 -0400

    merge transloadit uploading into upload clip views

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,6 +1,7 @@
 class ClipsController < ApplicationController
   before_action :authenticate_user
   before_action :set_user
+  include Transloadit::Rails::ParamsDecoder
 
   def new
     @clip = Clip.new
@@ -14,11 +15,19 @@ class ClipsController < ApplicationController
 
 
   def create
-    @clip = Clip.new(clip_params)
-    if @clip.save
-      redirect_to root_path, notice: "Clip successfully made"
+    Rails.logger.info("PARAMS: #{params[:transloadit].inspect}")
+    url = params[:transloadit][:results][:mp3][0]["url"]
+    duration = params[:transloadit][:results][:mp3][0][:meta]["duration"]
+    if duration <= 10.00
+      @clip = Clip.new(clip_params)
+      @clip.uploaded_file = url
+      if @clip.save
+        redirect_to root_path, notice: "Clip successfully made"
+      else
+        render :new, as: :new_file, notice: "Clip failed to upload"
+      end
     else
-      render :new, as: :new_file, notice: "Clip failed to upload"
+      redirect_to search_path
     end
   end
 
