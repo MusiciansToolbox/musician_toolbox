3ae0ea11fa40d78bd5a291a11db41e212b3ff549
TurnerMooreDavis <turnermdavis@gmail.com>
2015-07-20 13:13:56 -0400

    add back in modal line

--- app/assets/javascripts/tree_builder_test.js
+++ app/assets/javascripts/tree_builder_test.js
@@ -187,6 +187,7 @@ function treeBuilderTest(jam_circle_id) {
     var clip_id = d.clip_id;
     console.log($('#clip'+clip_id));
     $('#clip'+clip_id).click();
+    $('#myModal').modal('show');
   }
 
 
