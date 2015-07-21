1e8ebe4f6a7be878654ec9bd31e0e0a4996ccb9c
Cruz <nunez.a.cruz@gmail.com>
2015-07-21 12:39:27 -0400

    Remove excess js file

--- app/assets/javascripts/tree_builder.js
+++ app/assets/javascripts/tree_builder.js
@@ -1,165 +1,154 @@
-function treeBuilder() {
-  var margin = {
-      top: 20,
-      right: 120,
-      bottom: 20,
-      left: 120
-    },
-    width = 960 - margin.right - margin.left,
-    height = 800 - margin.top - margin.bottom;
+function treeBuilder(jam_circle_id) {
+  var ww = $(window).width(),
+      wh = $(window).height(),
+      sw = $('svg').width(),
+      sh = $('svg').height(),
+      radius = wh*.01,
+      i = 0,
+      duration = 750,
+      root;
 
-  var i = 0,
-    duration = 750,
-    root;
+
+  $('svg').replaceWith('<svg></svg>')
+
+  var svg = d3.select('body svg')
 
   var tree = d3.layout.tree()
-    .size([height, width]);
+    .size([sh,sw]);
 
   var diagonal = d3.svg.diagonal()
-    .projection(function(d) {
-      return [d.y, d.x];
-    });
+    .projection(function(d){return [ d.y , d.x ];});
 
-  var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  d3.json("/jam_circles/" + jam_circle_id + ".json", function(error, flare) {
-    if (error) throw error;
+  d3.json("/tree_data/" + jam_circle_id + ".json", function(data) {
+    root = data
+    var nodes = tree.nodes(data),
+        links = tree.links(nodes);
 
-    root = flare;
-    root.x0 = height / 2;
-    root.y0 = 0;
-
-    function collapse(d) {
-      if (d.children) {
-        d._children = d.children;
-        d._children.forEach(collapse);
-        d.children = null;
-      }
+    function drawLinks(){
+      svg.selectAll(".link")
+      .data(links)
+      .enter()
+      .append("path")
+      .attr("class", "link")
+      .attr("d", diagonal);
     }
 
-    root.children.forEach(collapse);
-    update(root);
-  });
+    function setNodeLocations() {
+      node = svg.selectAll('.node')
+        .data(nodes)
+        .enter()
+        .append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")";})
+          .attr("clip_id", function(d) {return d.id} )
+          .on("click", retrieveClip);
+          // .on("click", click);
+    }
 
-  d3.select(self.frameElement).style("height", "800px");
+    function drawCirclesAtNodeLocations() {
+    node.append("circle")
+      .attr("r", radius)
+    };
+
+    function addText() {
+      node.append("text")
+        .text(function(d) {return d.name;})
+        .attr("x", function(d) { return d.children || d._children ? -20 : 20; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+    }
+
+    function drawEverthing() {
+      drawLinks();
+      setNodeLocations();
+      drawCirclesAtNodeLocations();
+      addText();
+    }
+
+    drawEverthing();
+
+  })
 
   function update(source) {
 
     // Compute the new tree layout.
     var nodes = tree.nodes(root).reverse(),
-      links = tree.links(nodes);
+        links = tree.links(nodes);
 
     // Normalize for fixed-depth.
-    nodes.forEach(function(d) {
-      d.y = d.depth * 180;
-    });
+    // nodes.forEach(function(d) { d.y = d.depth * 400; });
 
     // Update the nodes…
     var node = svg.selectAll("g.node")
-      .data(nodes, function(d) {
-        return d.id || (d.id = ++i);
-      });
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
     // Enter any new nodes at the parent's previous position.
     var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) {
-        return "translate(" + source.y0 + "," + source.x0 + ")";
-      })
-      .on("click", click);
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+        // .on("click", click);
 
     nodeEnter.append("circle")
-      .attr("r", 1e-6)
-      .style("fill", function(d) {
-        return d._children ? "lightsteelblue" : "#fff";
-      });
+        .attr("r", radius)
+        .style("fill", function(d) { return d._children ? "black" : "#fff"; });
 
     nodeEnter.append("text")
-      .attr("x", function(d) {
-        return d.children || d._children ? -10 : 10;
-      })
-      .attr("dy", ".35em")
-      .attr("text-anchor", function(d) {
-        return d.children || d._children ? "end" : "start";
-      })
-      .text(function(d) {
-        return d.name;
-      })
-      .style("fill-opacity", 1e-6);
+        .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1e-6);
 
     // Transition nodes to their new position.
     var nodeUpdate = node.transition()
-      .duration(duration)
-      .attr("transform", function(d) {
-        return "translate(" + d.y + "," + d.x + ")";
-      });
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
     nodeUpdate.select("circle")
-      .attr("r", 4.5)
-      .style("fill", function(d) {
-        return d._children ? "lightsteelblue" : "#fff";
-      });
+        .attr("r", radius)
+        .style("fill", function(d) { return d._children ? "black" : "#fff"; });
 
     nodeUpdate.select("text")
-      .style("fill-opacity", 1);
+        .style("fill-opacity", 1);
 
     // Transition exiting nodes to the parent's new position.
     var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) {
-        return "translate(" + source.y + "," + source.x + ")";
-      })
-      .remove();
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+        .remove();
 
     nodeExit.select("circle")
-      .attr("r", 1e-6);
+        .attr("r", radius);
 
     nodeExit.select("text")
-      .style("fill-opacity", 1e-6);
+        .style("fill-opacity", 1e-6);
 
     // Update the links…
     var link = svg.selectAll("path.link")
-      .data(links, function(d) {
-        return d.target.id;
-      });
+        .data(links, function(d) { return d.target.id; });
 
     // Enter any new links at the parent's previous position.
     link.enter().insert("path", "g")
-      .attr("class", "link")
-      .attr("d", function(d) {
-        var o = {
-          x: source.x0,
-          y: source.y0
-        };
-        return diagonal({
-          source: o,
-          target: o
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
         });
-      });
 
     // Transition links to their new position.
     link.transition()
-      .duration(duration)
-      .attr("d", diagonal);
+        .duration(duration)
+        .attr("d", diagonal);
 
     // Transition exiting nodes to the parent's new position.
     link.exit().transition()
-      .duration(duration)
-      .attr("d", function(d) {
-        var o = {
-          x: source.x,
-          y: source.y
-        };
-        return diagonal({
-          source: o,
-          target: o
-        });
-      })
-      .remove();
+        .duration(duration)
+        .attr("d", function(d) {
+          var o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+        })
+        .remove();
 
     // Stash the old positions for transition.
     nodes.forEach(function(d) {
@@ -167,6 +156,7 @@ function treeBuilder() {
       d.y0 = d.y;
     });
   }
+
 
   // Toggle children on click.
   function click(d) {
@@ -179,4 +169,16 @@ function treeBuilder() {
     }
     update(d);
   }
+
+  function retrieveClip(d) {
+    var clip_id = d.clip_id;
+    var clip_url = d.clip_url
+    $('#myModal').modal('show');
+    $('#play-button').attr('value', clip_id);
+    $('#add-to-button').attr('value', clip_id);
+    $('#download-button').attr('href', clip_url);
+  }
+
+
+
 };
